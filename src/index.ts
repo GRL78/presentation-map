bf6513023b39d72ec1c460ffc404b4e5fd03690b
GRL <g.lesniewski@thecodingmachine.com>
2021-07-21 09:35:39 +0200

    Fix image that doesn't exist Update script to use the new API

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,70 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-var zoneOfficeName = "popupOfficeZone";
-var zoneEventName = "popupEventZone";
-var zoneSchoolName = "popupSchoolZone";
-var zoneTCMName = "popupTCMZone";
+const zoneOfficeName = "popupOfficeZone";
+const zoneEventName = "popupEventZone";
+const zoneSchoolName = "popupSchoolZone";
+const zoneTCMName = "popupTCMZone";
 
-var urlPricing = "https://workadventu.re/pricing";
-var urlSchoolOffer = "https://workadventu.re/school-offer";
-var urlEvent = "https://workadventu.re/events";
-var urlGettingStarted = "https://workadventu.re/getting-started";
+const urlPricing = "https://workadventu.re/pricing";
+const urlSchoolOffer = "https://workadventu.re/school-offer";
+const urlEvent = "https://workadventu.re/events";
+const urlGettingStarted = "https://workadventu.re/getting-started";
 
-var currentPopup : any = undefined;
+let currentPopup: any = undefined;
 
-WA.onEnterZone(zoneOfficeName, () => {
-    currentPopup =  WA.openPopup("popUpOffice","Create your own virtual office in WorkAdventure, it's easy !",[
+WA.room.onEnterZone(zoneOfficeName, () => {
+    currentPopup =  WA.ui.openPopup("popUpOffice","Create your own virtual office in WorkAdventure, it's easy !",[
         {
             label: "See the pricing",
             className: "normal",
-            callback: (popup => {
-                WA.openTab(urlPricing);
+            callback: (() => {
+                WA.nav.openTab(urlPricing);
             })
         },
         {
             label : "Getting Started",
             className :"normal",
-            callback : (popup => {
-                WA.openTab(urlGettingStarted);
+            callback : (() => {
+                WA.nav.openTab(urlGettingStarted);
             })
 
         }]);
 })
 
-WA.onEnterZone(zoneEventName, () => {
-    currentPopup =  WA.openPopup("popUpEvent","You can create your own Event in WorkAdventure",[
+WA.room.onEnterZone(zoneEventName, () => {
+    currentPopup =  WA.ui.openPopup("popUpEvent","You can create your own Event in WorkAdventure",[
         {
             label: "See the event page",
             className: "normal",
-            callback: (popup => {
-                WA.openTab(urlEvent);
+            callback: (() => {
+                WA.nav.openTab(urlEvent);
             })
         }]);
 })
 
-WA.onEnterZone(zoneTCMName, () => {
-    currentPopup =  WA.openPopup("popUpTCM","Come meet the WorkAdventure team in our office ! " +
+WA.room.onEnterZone(zoneTCMName, () => {
+    currentPopup =  WA.ui.openPopup("popUpTCM","Come meet the WorkAdventure team in our office ! " +
         " We are open from 9 am to 7pm CEST.",[]);
 })
 
 
-WA.onEnterZone(zoneSchoolName, () => {
-    currentPopup =  WA.openPopup("popUpSchool","WorkAdventure is free for teachers a the moment ! Take advantage of our offer ",[
+WA.room.onEnterZone(zoneSchoolName, () => {
+    currentPopup =  WA.ui.openPopup("popUpSchool","WorkAdventure is free for teachers a the moment ! Take advantage of our offer ",[
         {
             label: "See the offer",
             className: "normal",
-            callback: (popup => {
-                WA.openTab(urlSchoolOffer);
+            callback: (() => {
+                WA.nav.openTab(urlSchoolOffer);
             })
         }]);
 })
 
-WA.onLeaveZone(zoneSchoolName, closePopUp)
+WA.room.onLeaveZone(zoneSchoolName, closePopUp)
 
-WA.onLeaveZone(zoneTCMName, closePopUp)
+WA.room.onLeaveZone(zoneTCMName, closePopUp)
 
-WA.onLeaveZone(zoneEventName, closePopUp)
+WA.room.onLeaveZone(zoneEventName, closePopUp)
 
-WA.onLeaveZone(zoneOfficeName, closePopUp)
+WA.room.onLeaveZone(zoneOfficeName, closePopUp)
 
 function closePopUp(){
     if (currentPopup !== undefined) {
